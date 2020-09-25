78cea463571d2278ed027ce30b2022b07466739d
jetla <klau22@sjnd.org>
2020-09-25 10:13:53 -0700

    MoreStrings

---
+++ students/jet/GradeCalculator.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class GradeCalculator {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a decimal grade.");
+        double score = input.nextDouble();
+        System.out.println("The score you entered is: " + score);
+
+        if (score >= 94);
+        {
+            System.out.println("The letter grade is: A");
+        }
+
+
+    }
+}
+
