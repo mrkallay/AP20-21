c2feb23a9d8e8824609cdb6dd9bd4710c3e7e75a
jetla <klau22@sjnd.org>
2020-10-19 20:35:12 -0700

    MoreStrings

--- students/jet/ForLoopPractice.java
+++ students/jet/ForLoopPractice.java
@@ -3,30 +3,39 @@ public class ForLoopPractice {
 
     public static void main(String[] args) {
 
-        for (int i = 0; i <= 9999; i++){
+        for (int i = 0; i <= 9999; i++) {
             System.out.println(i);
 
         }
 
-        for (int i =10000; i >= -1; i--){
+        for (int i = 10000; i >= -1; i--) {
             System.out.println(i);
         }
 
-        for (int i = 0; i <=1000; i++){
-            if(i%2 == 0)
+        for (int i = 0; i <= 1000; i++) {
+            if (i % 2 == 0)
                 continue;
-            else{
+            else {
                 System.out.println(i);
             }
 
         }
         String stringOne = new String("The quick brown fox jumped over the lazy dog");
-        for(int i = 0; i <= stringOne.length() -1; i++){
+        for (int i = 0; i <= stringOne.length() - 1; i++) {
             System.out.println(stringOne.charAt(i));
         }
-        for(int count = stringOne.length()-1; count >= 0; count--)
-        {
+        for (int count = stringOne.length() - 1; count >= 0; count--) {
             System.out.println(stringOne.charAt(count));
         }
+        int vowelCount = 0;
+        for (int i = 0; i <= stringOne.length() - 1; i++) {
+            char vowel = stringOne.charAt(i);
+            if (vowel == 'a' || vowel == 'e' || vowel == 'i' || vowel == 'o' || vowel == 'u') {
+                vowelCount++;
+
+
+            }
+        }
+        System.out.println("There are " + vowelCount + " vowels in this sentence.");
     }
 }
