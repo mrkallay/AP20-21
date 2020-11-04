cad9eefa1abad0518680b9e7817aa73322948e6a
jetla <klau22@sjnd.org>
2020-11-04 10:27:13 -0800

    s

---
+++ students/jet/MathPractice.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+public class MathPractice {
+
+    public static void main(String[] args)
+    {
+        // 1) Instantiate a new scanner object for reading input from the user
+
+        System.out.println("Enter a integer number");
+        Scanner scan = new Scanner(System.in);
+
+
+
+        // 2) Prompt the user to enter an integer and use the scanner object to assign it to a variable
+
+        int num = scan.nextInt();
+
+
+        // 3) Use the Math class to get the absolute value of the entered integer and display
+        //    the result to the screen.  Be sure to test with negative values
+
+        System.out.println(Math.abs(num));
+
+        // 3) Prompt the user twice for integers and assign them to two separate variables
+        System.out.println("Enter a integer number");
+        Scanner scan1 = new Scanner(System.in);
+        int num2 = scan.nextInt();
+
+        System.out.println("Enter a integer number");
+        Scanner scan3 = new Scanner(System.in);
+        int num3 = scan.nextInt();
+
+
+        // 4) Without using conditional logic display the larger of the integers to the screen
+
+
+
+        // 5) Prompt the user to enter a decimal number (double) and use the scanner object to assign it to a variable
+
+        System.out.println("Enter a decimal number");
+        Scanner scan4 = new Scanner(System.in);
+        double num4 = scan.nextInt();
+
+        // 6) Use the Math class to round the double up and down to the nearest double (see floor/ceil)
+
+
+    }
+}
