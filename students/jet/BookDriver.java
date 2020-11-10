122689eb69c7b7ea238bd0f89ed66cbc2cffc90b
JetLau002 <klau22@sjnd.org>
2020-11-10 12:11:09 -0800

    s

---
+++ students/jet/BookDriver.java
@@ -0,0 +1,10 @@
+public class BookDriver
+{
+    public static void main(String[] args)
+    {
+        Book b = new Book();
+        b.setTitle("Running with Sherman");
+        b.setPages(220);
+        System.out.println(b.getInfo());
+    }
+}
